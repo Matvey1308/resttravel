0c12bb5327aea15583a97a4d827c3d78997eb15f
Vladyslav Matvieiev <Matvey1308@gmail.com>
2017-10-22 13:27:19 +0300

    no message

--- js/common.js
+++ js/common.js
@@ -38,54 +38,9 @@ $(function() {
 
 
 	// When the window has finished loading create our google map below
-   google.maps.event.addDomListener(window, 'load', init);
+   // google.maps.event.addDomListener(window, 'load', init);
 
-   function init() {
-	   // Basic options for a simple Google Map
-	   // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-	   var contentString = '<div style="overflow: auto;">' +
-'	   <div style="margin-bottom: 3px;">' +
-'	   <strong>Taurus Hotel &amp; Spa</strong>' +
-	   '</div>' +
-	   '<div>пл. Князя Святослава, 5, Львів, Львівська область, 79000</div>' +
-	   '</div>';
-	   var mapOptions = {
-		   // How zoomed in you want the map to start at (always required)
-		   zoom: 11,
 
-		   // The latitude and longitude to center the map (always required)
-		   center: new google.maps.LatLng(49.842367,24.0072618), // New York
-
-		   // How you would like to style the map.
-		   // This is where you would paste any style found on Snazzy Maps.
-		   styles: [{"featureType":"all","elementType":"geometry.fill","stylers":[{"color":"#ff0000"}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#424b5b"},{"weight":2},{"gamma":"1"}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"weight":0.6},{"color":"#545b6b"},{"gamma":"0"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#ffffff"},{"visibility":"on"},{"lightness":"0"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#545b6b"},{"gamma":"1"},{"weight":"10"}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#121318"}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#2c2e3d"}]},{"featureType":"landscape.man_made","elementType":"geometry.stroke","stylers":[{"visibility":"on"},{"weight":"2.00"}]},{"featureType":"landscape.man_made","elementType":"labels.text.fill","stylers":[{"color":"#ff0000"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"color":"#111428"}]},{"featureType":"landscape.natural.landcover","elementType":"geometry.fill","stylers":[{"color":"#111428"},{"visibility":"on"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry.fill","stylers":[{"color":"#111428"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#666c7b"}]},{"featureType":"poi.attraction","elementType":"labels.icon","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","elementType":"labels.icon","stylers":[{"visibility":"on"}]},{"featureType":"poi.government","elementType":"labels.icon","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","elementType":"labels.icon","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#545b6b"}]},{"featureType":"poi.place_of_worship","elementType":"labels.icon","stylers":[{"visibility":"on"}]},{"featureType":"poi.school","elementType":"labels.icon","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","elementType":"labels.icon","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"geometry","stylers":[{"color":"#424a5b"},{"lightness":"0"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#808399"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#666c7b"}]},{"featureType":"transit","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#71718c"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#ffffff"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#515a70"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#414560"}]},{"featureType":"water","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"visibility":"on"}]},{"featureType":"water","elementType":"labels.text","stylers":[{"color":"#ffffff"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"visibility":"on"}]}]
-	   };
-	   var infowindow = new google.maps.InfoWindow({
-   content: contentString
- });
-	   // Get the HTML DOM element that will contain your map
-	   // We are using a div with id="map" seen below in the <body>
-	   var mapElement = document.getElementById('map');
-
-	   // Create the Google Map using our element and options defined above
-	   var map = new google.maps.Map(mapElement, mapOptions);
-
-	   // Let's also add a marker while we're at it 70 80
-	   var marker = {
-		   url: '../img/mark.png',
-	   };
-
-	   // NOW CREATE THE ACTUAL MARKER ON THE MAP
-	   var myMarker = new google.maps.Marker({
-		   position: new google.maps.LatLng(49.842367,24.0072618),
-		   map: map,
-		   icon: marker
-	   });
-	   myMarker.addListener('click', function() {
-   infowindow.open(map, myMarker);
- });
-
-   }
 
    var groups = {};
    $('.galleryItem').each(function() {
@@ -112,5 +67,17 @@ $(function() {
 
    });
 
+   $('nav a').click(function (e) {
+	   e.preventDefault();
+	   var slide = $(this).attr('data-slide');
+	   $('html, body').animate({
+		   scrollTop: $('div[data-slide= '+ slide + ']').offset().top - $('div.navigation').outerHeight()
+	   }, 1000);
+   });
+  	$('.btn.btn-white').click(function () {
+		$('html, body').animate({
+ 		   scrollTop: $('div[data-slide=slide6]').offset().top
+ 	   }, 1000);
+  	});
 
 });
